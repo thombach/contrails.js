3f405c14195ce41e9a6096fad104bcc8de4e797a
Thomas Bach <thomasbach38@gmail.com>
2022-06-12 22:18:48 +0200

    Particles class

--- contrails.js
+++ contrails.js
@@ -3,11 +3,14 @@
  * Github : https://github.com/thomasbachdev/contrails.js
  */
 
-import params from "./contrails.json" assert { type: "json" };
+import parameters from "./contrails.json" assert { type: "json" };
+
+const params = parameters;
 
 const TO_RADIANS = Math.PI / 180;
-const fps = params['fps'];
-const canvasOffset = params['canvas_offset'];
+const fps = params.fps;
+const canvasOffset = params.canvas_offset;
+const maxParticleNum = params.particles.number;
 
 const contrails = document.getElementById("contrails");
 contrails.style.width = "100%";
@@ -26,14 +29,45 @@ const ctx = canvas.getContext("2d");
 
 /* Init plane image */
 var planeImg = new Image();
-planeImg.src = params["img_path"];
+planeImg.src = params.img_path;
 
+class Particle {
+  constructor(position) {
+    this.position = position;
+    this.size = params.particles.size;
+    this.lifespan = params.particles.lifespan;
+    this.life = this.lifespan;
+    this.opacity = 1;
+    this.color = params.particles.color;
+  }
+
+  draw() {
+    ctx.save();
+    ctx.fillStyle = params.particles.color;
+    ctx.globalAlpha = this.opacity;
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.restore();
+  }
+
+  update() {
+    if(this.life > 0){
+      this.draw();
+      this.life--;
+      this.opacity = this.life / this.lifespan;
+    }
+  }
+}
+
+/* Plane object */
 class Plane {
-  constructor(position, speed, size, angle) {
+  constructor(position, angle) {
     this.pos = position;
-    this.speed = speed;
-    this.size = size;
+    this.speed = params.planes.speed;
+    this.size = params.planes.size;
     this.angle = angle;
+    this.contrail = [];
   }
 
   move() {
@@ -57,11 +91,6 @@ class Plane {
     this.pos.y += Math.sin(this.angle * TO_RADIANS) * this.speed;
   }
 
-  ajust() {
-    if (-canvasOffset > this.pos.x)
-    return -canvasOffset > this.pos.x || -canvasOffset > this.pos.y || canvasOffset < this. pos.x || canvasOffset < this.pos.y;
-  }
-
   draw() {
     ctx.save();
     ctx.translate(this.pos.x, this.pos.y);
@@ -79,12 +108,23 @@ class Plane {
   update() {
     this.move();
     this.draw();
+
+    if (this.contrail.length < maxParticleNum) {
+      this.contrail.push(new Particle(this.position));
+    }
+    this.contrail.forEach((particle, index) => {
+      particle.update();
+      if(particle.life <= 0){
+        this.contrail.splice(index, 1);
+      }
+    })
   }
 }
 
+/* Create and manage the planes */
 class PlaneManager {
-  constructor(number) {
-    this.number = number;
+  constructor() {
+    this.number = params.planes.number;
     this.planes = [];
     for (let index = 0; index < this.number; index++) {
       this.createPlane();
@@ -96,10 +136,8 @@ class PlaneManager {
       x: Math.random() * canvas.width,
       y: Math.random() * canvas.height,
     };
-    let speed = params["planes"]["speed"];
-    let size = params["planes"]["size"];
     let angle = Math.random() * 360;
-    this.planes.push(new Plane(position, speed, size, angle));
+    this.planes.push(new Plane(position, angle));
   }
 
   update() {
@@ -109,7 +147,7 @@ class PlaneManager {
   }
 }
 
-var pm = new PlaneManager(params["planes"]["number"]);
+var pm = new PlaneManager();
 
 function animate() {
   setTimeout(function () {
